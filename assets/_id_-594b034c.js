4c5979ba702bd319340ec499b406857abedea50a
poqopo <block.poqopo@gmail.com>
2025-02-13 14:50:29 +0900

    Updates

---
+++ assets/_id_-594b034c.js
@@ -0,0 +1,1 @@
+import{u as i,j as s}from"./index-24be79eb.js";function r(){const e=i();return s.jsx("div",{children:s.jsxs("h1",{children:["demo detail, demo id is ",e.id]})})}export{r as default};
