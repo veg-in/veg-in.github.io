e3d83b9d019b5ff926f2dd301db3f3a52d196890
poqopo <block.poqopo@gmail.com>
2025-02-13 14:14:39 +0900

    gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ export function App() {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <App />
       </Router>
     </QueryClientProvider>
