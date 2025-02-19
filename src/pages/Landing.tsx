0c6107670429b4894411a0828de0579ef4507dde
ddol9 <drddol9@gmail.com>
2025-02-19 16:42:16 +0900

    style: update landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,19 +4,19 @@ const Landing = () => {
   return (
     <>
       <div className='flex gap-8 mb-12 mt-8'>
-        <div className='text-7xl'>🔥</div>
+        <div className='text-7xl'>🦅</div>
       </div>
-      <h1 className='text-4xl font-bold mb-8'>Welcome to Our Site</h1>
+      <h1 className='text-4xl font-bold mb-8'>DOKPAMI</h1>
       <div className='flex gap-4'>
         <Link
           to='/about'
-          className='px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors'
+          className='px-4 py-2 bg-[#FFCE00] text-black rounded-lg border-2 border-gray-950 hover:bg-[#E6B800] transition-colors'
         >
           소개 페이지
         </Link>
         <Link
           to='/quiz'
-          className='px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition-colors'
+          className='px-4 py-2 bg-[#FF0000] text-white rounded-lg hover:bg-[#CC0000] transition-colors'
         >
           퀴즈 페이지
         </Link>
@@ -26,4 +26,3 @@ const Landing = () => {
 };
 
 export default Landing;
-`ÍÍ`;
