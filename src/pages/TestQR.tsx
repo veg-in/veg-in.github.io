065c07a2b4b294ef2d523783ce7d8e18d62b00bd
ddol9 <drddol9@gmail.com>
2025-02-25 17:10:52 +0900

    임시저장

--- src/pages/TestQR.tsx
+++ src/pages/TestQR.tsx
@@ -1,12 +1,27 @@
-// import { useState } from 'react';
+import useKakaoLoader from '@/components/kakaomap/useKakaoLoader';
+import KakaoMap from '@/components/kakaomap/Map';
+import { Map } from 'react-kakao-maps-sdk';
 
 export default function TestQR() {
-    // const [value, setValue] = useState('');
-  
-    return (
-      <div className=''>
-        <div>여기에서 QR 테스트 진행해주세요</div>
-      </div>
-    );
-  }
-  
+  useKakaoLoader();
+
+  return (
+    <div>
+      <Map // 지도를 표시할 Container
+        id='map'
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          // 지도의 크기
+          width: '100%',
+          height: '350px',
+        }}
+        level={3} // 지도의 확대 레벨
+      />
+      <KakaoMap />
+    </div>
+  );
+}
