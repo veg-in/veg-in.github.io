7fdfce8ea2a31d3601768625f276aea35e9d36d8
guboc11 <guboc11@gmail.com>
2025-02-27 20:08:59 +0900

    Rename quiz -> balancegame page

---
+++ src/pages/BalanceGame.tsx
@@ -0,0 +1,9 @@
+const BalanceGame = () => {
+  return (
+    <>
+      <h1 className='text-4xl font-bold mb-8'>대학 생활 밸런스 게임</h1>
+    </>
+  );
+};
+
+export default BalanceGame;
--- src/pages/Quiz.tsx
+++
@@ -1,9 +0,0 @@
-const Quiz = () => {
-  return (
-    <>
-      <h1 className='text-4xl font-bold mb-8'>퀴즈 페이지</h1>
-    </>
-  );
-};
-
-export default Quiz;
