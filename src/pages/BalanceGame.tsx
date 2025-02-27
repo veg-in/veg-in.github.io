73ca93ca3e9415db53c1fec432f4eac008e8dfaf
guboc11 <guboc11@gmail.com>
2025-02-27 21:42:32 +0900

    Add balance game buttons

--- src/pages/BalanceGame.tsx
+++ src/pages/BalanceGame.tsx
@@ -2,6 +2,23 @@ const BalanceGame = () => {
   return (
     <>
       <h1 className='text-4xl font-bold mb-8'>대학 생활 밸런스 게임</h1>
+
+      <h2 className='text-xl mb-8'>1/11</h2>
+      <h2 className='text-xl mb-16'>대학생활 첫 걸음! 수강 신청, 어떻게 할래?</h2>
+      <div className='flex flex-col space-y-16'>
+        <button
+          className='cursor-pointer py-2 w-64 border-2 border-black bg-yellow-400 active:bg-yellow-700 rounded-lg'
+          onClick={() => {}}
+        >
+          21학점 듣고 조기 졸업
+        </button>
+        <button
+          className='cursor-pointer py-2 w-64 border-2 border-black bg-yellow-400 active:bg-yellow-700 rounded-lg '
+          onClick={() => {}}
+        >
+          15학점 듣고 초과 학기
+        </button>
+      </div>
     </>
   );
 };
