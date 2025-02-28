40cd52ed1c59b0132f4918039127f4f619e66013
ddol9 <drddol9@gmail.com>
2025-02-28 11:05:40 +0900

    fix router

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className='max-w-[600px] h-[1080px] mx-auto bg-[#88D0E5] flex flex-col items-center justify-start p-8 overflow-y-auto'>
+    <div className='max-w-[600px] min-h-screen mx-auto bg-[#88D0E5] flex flex-col items-center justify-start p-8 overflow-y-auto'>
       <Outlet />
     </div>
   );
--- src/pages/AboutNext.tsx
+++ src/pages/AboutNext.tsx
@@ -1,25 +1,26 @@
 import { Link } from 'react-router-dom';
 
 export default function AboutNext() {
-  
-    return (
-      <div className='relative'>
-        <div className="font-bold text-center">소개문구 (남들과 다르다는건 알겠는데 난 대체 누구지? 싱나비 과대 독팜희와 함께, 이 낯선 친구를 연대생으로 만들어보세요!)</div>
-        <div className='flex justify-center items-end'>
-            <img className='w-full h-auto' src='grassland.png' />
-        </div>
-
-        <Link to='/quiz'>
-            <button className="absolute bottom-28 left-1/2 w-76 h-14 transform -translate-x-1/2 bg-[#FFCE00] border-2 border-black rounded hover:bg-blue-500 text-lg font-bold">
-            연대생 변신 시작! 너의 선택은?
-            </button>
-        </Link>
-        <Link to='/testqr'>
-            <button className="absolute bottom-8 left-1/2 w-76 h-14 transform -translate-x-1/2 bg-[#FFCE00] border-2 border-black rounded hover:bg-blue-500 text-lg font-bold">
-            동아리 박람회 이벤트 참여하기
-            </button>
-        </Link>
+  return (
+    <div className='relative'>
+      <div className='font-bold text-center'>
+        소개문구 (남들과 다르다는건 알겠는데 난 대체 누구지? 싱나비 과대 독팜희와 함께, 이 낯선
+        친구를 연대생으로 만들어보세요!)
       </div>
-    );
-  }
-  
+      <div className='flex justify-center items-end'>
+        <img className='w-full h-auto' src='grassland.png' />
+      </div>
+
+      <Link to='/balancegame'>
+        <button className='absolute bottom-28 left-1/2 w-76 h-14 transform -translate-x-1/2 bg-[#FFCE00] border-2 border-black rounded hover:bg-blue-500 text-lg font-bold'>
+          연대생 변신 시작! 너의 선택은?
+        </button>
+      </Link>
+      <Link to='/testqr'>
+        <button className='absolute bottom-8 left-1/2 w-76 h-14 transform -translate-x-1/2 bg-[#FFCE00] border-2 border-black rounded hover:bg-blue-500 text-lg font-bold'>
+          동아리 박람회 이벤트 참여하기
+        </button>
+      </Link>
+    </div>
+  );
+}
