d5a79d3462445240de0e775903812663a4770680
guboc11 <guboc11@gmail.com>
2025-03-02 14:19:07 +0900

    Add WalletProvider & Install related libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import Routers from './Routers';
+import { SuiClientProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WalletProvider } from '@mysten/dapp-kit';
+
+const queryClient = new QueryClient();
+const networks = {
+  devnet: { url: getFullnodeUrl('devnet') },
+  testnet: { url: getFullnodeUrl('testnet') },
+  testnet_suiscan: { url: 'https://rpc-testnet.suiscan.xyz:443' },
+  mainnet: { url: getFullnodeUrl('mainnet') },
+};
 
 function App() {
-  return <Routers></Routers>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork='testnet_suiscan'>
+        <WalletProvider autoConnect>
+          <Routers />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
