914fd30b53a94cca1f340a80f426188f0b3c8b5a
guboc11 <guboc11@gmail.com>
2025-03-02 20:59:12 +0900

    Update ResultPage : add image url from firebase storage

--- src/pages/BalanceGame/_ResultPage.tsx
+++ src/pages/BalanceGame/_ResultPage.tsx
@@ -1,47 +1,22 @@
 import { BalanceGameResults } from '@/data/BalanceGameResult';
-// import { storage } from '@/lib/firebase';
-// import { ref, getDownloadURL } from 'firebase/storage';
-
 interface ResultPageProps {
   finalResult: string;
 }
 export default function ResultPage({ finalResult }: ResultPageProps) {
-  // useEffect(() => {
-  //   const storageRef = ref(storage, 'yonsei.png');
-  //   console.log(storageRef);
-  //   const gb = async () => {
-  //     // const blob = await getBlob(storageRef);
-  //     // console.log(blob);
-  //     getDownloadURL(storageRef).then((url) => {
-  //       console.log(url); // 올바른 URL 출력됨
-  //     });
-  //   };
-  //   gb();
-  // });
+  const resultInfo = BalanceGameResults[finalResult];
 
-  // finalResult 값을 기반으로 올바른 키를 찾습니다
-  const resultKey = Object.keys(BalanceGameResults).find((key) => key.includes(finalResult));
-
-  // 찾은 키가 있으면 해당 정보를 사용하고, 없으면 기본값 사용
-  const info = resultKey
-    ? BalanceGameResults[resultKey]
-    : {
-        title: '결과를 찾을 수 없습니다',
-        subtitle: '',
-        participantRate: '',
-        content: '',
-      };
+  if (!resultInfo) return <div>결과를 찾을 수 없습니다.</div>;
 
   return (
     <>
       <div className='flex flex-col items-center'>
         <h1 className='mb-8'>내가 고른 대학 생활은...</h1>
       </div>
-      <img src='/result_dokpami.png' className='w-48 h-48 object-contain mb-4' />
-      <div className='font-bold text-xl mb-5'>{info.title}</div>
-      <div className='text-sm mb-4'>{info.subtitle}</div>
-      <div className='mb-2 text-sm'>{info.participantRate}</div>{' '}
-      <div className='mb-10 whitespace-pre-line text-sm'>{info.content}</div>{' '}
+      <img src={resultInfo.imageURL} className='w-48 h-48 object-contain mb-4' />
+      <div className='font-bold text-xl mb-5'>{resultInfo.title}</div>
+      <div className='text-sm mb-4'>{resultInfo.subtitle}</div>
+      <div className='text-sm mb-4'>전체 참여자 중 00%가 이 유형이에요!</div>
+      <div className='mb-10 whitespace-pre-line text-sm'>{resultInfo.content}</div>{' '}
       <div className='mb-3 text-sm text-center'>
         @dokpami.nft를 팔로우한 후 <br /> 태그해서 인스타 스토리를 올리면 oo을 받을 수 있어!
       </div>
