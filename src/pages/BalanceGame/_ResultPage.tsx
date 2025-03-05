07f21788b977f035962ce0f690ab9402c2032c08
hyo141 <hyochang141@gmail.com>
2025-03-05 21:45:15 +0900

    modify capture button to share button in balancegame result page

--- src/pages/BalanceGame/_ResultPage.tsx
+++ src/pages/BalanceGame/_ResultPage.tsx
@@ -6,10 +6,11 @@ import BalanceGameResultCapture from './_BalanceGameResultCapture';
 interface ResultPageProps {
   finalResult: string;
 }
+
 export default function ResultPage({ finalResult }: ResultPageProps) {
   const [totalTypeCount, setTotalTypeCount] = useState(0);
   const [typePercentage, setTypePercentage] = useState('');
-  const resultPageRef = useRef<HTMLDivElement | null>(null); // resultPageRef 추가
+  const resultPageRef = useRef<HTMLDivElement | null>(null);
   const [imgLoaded, setImgLoaded] = useState(false);
 
   const resultInfo = BalanceGameResults[finalResult];
@@ -35,6 +36,7 @@ export default function ResultPage({ finalResult }: ResultPageProps) {
           src={resultInfo.imageURL}
           id='result-img'
           className='w-48 h-48 object-contain mb-4'
+          crossOrigin='anonymous' // CORS 설정 추가
           onLoad={() => setImgLoaded(true)}
         />
         <div className='font-bold text-xl mb-5'>{resultInfo.title}</div>
@@ -42,14 +44,9 @@ export default function ResultPage({ finalResult }: ResultPageProps) {
         <div className='text-sm mb-4'>
           전체 {totalTypeCount}개의 결과 중 {typePercentage}%가 이 유형이에요!
         </div>
-        <div className='mb-10 whitespace-pre-line text-sm'>{resultInfo.content}</div>{' '}
+        <div className='mb-10 whitespace-pre-line text-sm'>{resultInfo.content}</div>
       </div>
-      <div className='mb-3 text-sm text-center'>
-        @dokpami.nft를 팔로우한 후 <br /> 태그해서 인스타 스토리를 올리면 oo을 받을 수 있어!
-      </div>
-      <div className='mb-10 text-sm font-bold'>인스타 스토리 이벤트 참여하기</div>
       {imgLoaded && <BalanceGameResultCapture resultPageRef={resultPageRef} />}
-      {/* 새 컴포넌트 추가 */}
       <button
         className='mb-8 font-bold text-sm bg-white border-2 border-black rounded px-3 py-2 hover:bg-blue-600'
         onClick={() => {}}
@@ -60,19 +57,3 @@ export default function ResultPage({ finalResult }: ResultPageProps) {
     </>
   );
 }
-
-// <>
-//   <div id='result-page' ref={resultPageRef} className='flex flex-col items-center'>
-//     <h1 className='mb-8'>내가 고른 대학 생활은...</h1>
-//     <img src='/result_dokpami.png' className='w-48 h-48 object-contain mb-4' />
-//     <div className='font-bold text-xl mb-5'>{info.title}</div>
-//     <div className='text-sm mb-4'>{info.subtitle}</div>
-//     <div className='mb-2 text-sm'>{info.participantRate}</div>
-//     <div className='mb-10 whitespace-pre-line text-sm'>{info.content}</div>
-//   </div>
-//   <BalanceGameResultCapture resultPageRef={resultPageRef} /> {/* 새 컴포넌트 추가 */}
-//   <button className='mb-8 font-bold text-sm bg-white border-2 border-black rounded px-3 py-2 hover:bg-blue-600'>
-//     Google Login하고 더 많은 이벤트 참여하기
-//   </button>
-//   <div className='text-sm font-bold'>BlockBlock x DOKPAMI</div>
-// </>
